6b131158ffa0c1377cc7a97d648054655eaad788
mariososadi <mariososadi@gmail.com>
2017-07-08 22:01:58 -0500

    Add files via upload

--- 02/05/multiple_linear_regression.py
+++ 02/05/multiple_linear_regression.py
@@ -10,32 +10,53 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-# Encoding categorical data
+# Encoding categorial data (category table)
+# Encoding the Independant Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Spitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, 
+                                                    random_state = 0)
+
+# Feature Scaling 
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X_test = sc_X.transform(X_test)"""
 
-# Fitting Multiple Linear Regression to the Training set
+# Fitting Miltiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(X_train,y_train)
 
-# Predicting the Test set results
+# Predicting the Test set Results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+
+X_opt = X[:]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+    
+while True:
+     
+    regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()    
+    val = np.amax(regressor_OLS.pvalues)    
+    if val > 0.05:
+        index_deleted = regressor_OLS.pvalues.tolist().index(val)
+        X_opt = np.delete(X_opt, index_deleted, axis=1)
+        
+    if val < 0.05:
+        break
+
+
